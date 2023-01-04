302f34f503f79115e6b1628e4e0843a65f364262
Kirill <grigorev@posteo.de>
2023-01-04 14:54:37 +0100

    wip

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	log.Println("started...")
-	for range time.NewTicker(5 * time.Second).C {
+	for range time.NewTicker(3 * time.Second).C {
 		fmt.Println("Hello there", time.Now())
 	}
 }
